a5e087cb170128da4006dcd68b8d5fdfc909f795
annita <e.annita2001@gmail.com>
2025-05-26 07:11:21 +0600

    dead f

--- alumni-client/src/components/homepage/alumni/AlumniList.jsx
+++ alumni-client/src/components/homepage/alumni/AlumniList.jsx
@@ -26,7 +26,7 @@ function AlumniList() {
 
   return (
     <div className="h-screen overflow-y-auto">
-      <div className="flex flex-col gap-4 mb-8">
+      <div className="flex flex-col gap-4 mb-8 bg-white">
       {Object.entries(sessions).map(([session, users]) => (
         <List key={session} session={session} users={users} />
       ))}
--- alumni-client/src/components/homepage/alumni/list-components/Line.jsx
+++ alumni-client/src/components/homepage/alumni/list-components/Line.jsx
@@ -5,11 +5,11 @@ function Line({  onCollapse, session}) {
     <div className="mt-10">
       <hr className="text-gray-300" />
       <div className=" flex justify-between mx-4 md:mx-10 transform -translate-y-4">
-        <p className="text-lg font-semibold bg-gray-600 rounded-2xl px-4">
+        <p className="text-lg font-semibold bg-gray-200 dark:bg-gray-600 rounded-2xl px-4">
           {session}
         </p>
         <button
-          className="text-lg bg-gray-600 rounded-2xl px-4"
+          className="text-lg bg-gray-300 cursor-pointer rounded-2xl px-4"
           onClick={onCollapse}>
           {" "}
           see all
